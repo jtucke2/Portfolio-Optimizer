02b047ff3cc629e41ed9e72a5e54f65de50c40fb
Jason Tucker <nottelling@something.com>
2019-03-09 17:15:33 -0500

    *

---
+++ README.md
@@ -0,0 +1,7 @@
+# Financial Portfolio Optimizer
+## Jason Tucker
+
+#### AIT 715 Case Study - Towson University - Spring 2019
+
+
+
---
+++ backend/server/app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_restplus import Resource, Api
+from make_celery import make_celery
+
+app = Flask(__name__)
+app.config.update(
+    CELERY_RESULT_BACKEND='mongodb://po-database:27017/tasks',
+    CELERY_BROKER_URL='pyamqp://admin:password@po-task-queue//'
+)
+celery = make_celery(app)
+api = Api(app, doc="/swagger")
+
+
+@celery.task(name="tasks.add_together")
+def add_together(a, b):
+    return a + b
+
+
+@api.route('/asset-prices')
+class AssetPrices(Resource):
+    def get(self):
+        add_together.delay(234, 234243)
+        return {'hello': 'world'}
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port="5000", host="0.0.0.0")
