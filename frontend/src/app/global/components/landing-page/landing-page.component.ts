feab4c97ae11b116ea82449e437f4072a7c4fae2
Jason Tucker <nottelling@something.com>
2019-04-29 20:56:17 -0400

    landing page, fixes #14

--- frontend/src/app/global/components/landing-page/landing-page.component.ts
+++ frontend/src/app/global/components/landing-page/landing-page.component.ts
@@ -1,17 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { UserService } from '../../services/user.service';
+import { Subject, interval } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent implements OnInit {
+export class LandingPageComponent implements OnInit, OnDestroy {
+  public onDestroy$ = new Subject<null>();
+  public percentMixers = [
+    {
+      aapl: 0.5,
+      googl: 0.2,
+      msft: 0.3,
+    },
+    {
+      aapl: 0.1,
+      googl: 0.8,
+      msft: 0.1,
+    },
+    {
+      aapl: 0.0,
+      googl: 0.3,
+      msft: 0.7,
+    }
+  ];
+  public percentMixer = this.percentMixers[0];
 
   constructor(public userService: UserService) { }
 
+  ngOnDestroy() {
+    this.onDestroy$.next();
+  }
+
   ngOnInit() {
+    let counter = 1;
+    interval(3000)
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe(() => {
+        this.percentMixer = this.percentMixers[counter % 3];
+        counter++;
+      });
   }
 
 }
